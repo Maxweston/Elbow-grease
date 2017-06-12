33945297102a115c2853b6cd19852c03ea75baa0
Max Weston <max@connected-ventures.com>
2017-06-12 15:34:50 +0100

    new boilerplate set up done

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var stylus = require('stylus');
+
+// gulp.src('src/templates/*.pug')
+//   .pipe(pug())
+//   .pipe(gulp.dest('dist'))
+
+// gulp.src('src/styles/*.styl')
+//   .pipe(stylus())
+//   .pipe(gulp.dest('dist/css'))
+
+// gulp.task('default', function() {
+  // place code for your default task here
+// });
+
+gulp.task('build:pug', function() {
+  var stream = gulp.src('src/templates/*.pug')
+  .pipe(pug())
+  .pipe(gulp.dest('dist'))
+})
+
+gulp.task('build:stylus', function() {
+  var stream = gulp.src('src/styles/*.styl')
+  .pipe(stylus())
+  .pipe(gulp.dest('dist'))
+})
