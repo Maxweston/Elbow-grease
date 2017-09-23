9a7f20de32b95208fefcea6f15941bde0e698eca
Max Weston <max.weston2@gmail.com>
2017-09-23 17:12:18 +0100

    added js watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,25 @@ var gulp = require('gulp');
 var pug = require('gulp-pug');
 var stylus = require('gulp-stylus');
 
-gulp.task('build:pug', function() {
+gulp.task('build:pug', function () {
   var stream = gulp.src('src/templates/index.pug')
   .pipe(pug())
   .pipe(gulp.dest('dist'))
 })
 
-gulp.task('build:stylus', function() {
+gulp.task('build:stylus', function () {
   var stream = gulp.src('src/styles/main.styl')
   .pipe(stylus())
+  .pipe(gulp.dest('dist'))
+})
+
+gulp.task('build:js', function () {
+  var stream = gulp.src('src/js/app.js')
   .pipe(gulp.dest('dist'))
 })
 
 gulp.task('watch', function() {
   gulp.watch('src/styles/*.styl', ['build:stylus'])
   gulp.watch('src/templates/*.pug', ['build:pug'])
+  gulp.watch('src/js/*.js', ['build:js'])
 })
